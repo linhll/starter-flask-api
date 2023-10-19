85fa98014e5e278c46c48a463706c804e6898b2d
linhnh <nhlinhcs@gmail.com>
2023-10-19 17:04:48 +0700

    fix float32 array

--- tiktok_captcha.py
+++ tiktok_captcha.py
@@ -62,8 +62,8 @@ print("model loaded")
 
 def get_classify(img):
     # img = PIL.Image.fromarray(img)
-    img_array = np.array(img, dtype=np.float32)
     img_array = np.float32(img_array)
+    img_array = np.array(img, dtype=np.float32)
     img_array = np.expand_dims(img, 0) # Create a batch
 
     prediction = classify_lite(rescaling_1_input=img_array)['dense_1']
